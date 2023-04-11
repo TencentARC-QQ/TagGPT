bf7e3408b17e709b27998443c9d530e9e440c255
palchenli <palchenli@tencent.com>
2023-04-11 11:12:10 +0800

    update

--- scripts/main.py
+++ scripts/main.py
@@ -3,11 +3,64 @@ import pandas as pd
 import tqdm
 import numpy as np
 import argparse
+import random
 
 from langchain import OpenAI, ConversationChain
 from langchain.prompts import PromptTemplate
 from collections import Counter
 from sentence_transformers import SentenceTransformer
+
+
+def format_data(data, preference):
+    examples = [
+        "例如，给定一个视频，它的\"标题\"为\"笼子挺高的百分之八十不会跳出去，不知道是不是被什么吃掉了，但是也没有看见血，继续寻找biubiu，你们有"
+        "没有找仓鼠的小办法\"，\"类别\"为\"动物\"，\"ocr\"为\"今天发现biubiu不见了,哪里都没有biubiu,昨天晚上笼子盖没有关\"，\"asr\"为\""
+        "今天发现BB我不见了，哪里都没有BB昨天晚上笼子盖没有关，应该是跑出去了，但是这个笼子很高，一般跑不出去，加油找b区b区吧。\"，{}生成机器人"
+        "推断出合理的\"{}\"为\"仓鼠伪冬眠、仓鼠假死、仓鼠不见了、仓鼠冬眠\"。".format(preference, preference),
+        "例如，给定一个视频，它的\"标题\"为\"不会画动漫腿？来看看你画的腿对不对 #动漫   #手绘教程   #手绘     #未来设计师\"，\"类别\"为\"才"
+        "艺\"，\"ocr\"为\"不会画好看的漫画腿,跟我学画腿这样画更好看\"，\"asr\"为\"所以住万三另外的小路看我像吗，我每天都要做这么像漫步又会忘"
+        "了身处妖精跳出物神秘的心情，要对全世界说，所以住万三另外在小路看我像吗？我和都要做这么像漫步又会忘了。\"，{}生成机器人推断出合理的\"{}"
+        "\"为\"动漫老师、动漫人物绘画教程、漫画腿怎么画、绘画新手教程\"。".format(preference, preference),
+        "例如，给定一个视频，它的\"标题\"为\"日常生活小技巧 #生活小妙招  #内蒙特产\"，\"类别\"为\"健康,生活\"，\"ocr\"为\"生活小妙招招\"，"
+        "\"asr\"为\"管的这些小技巧，知道你就捡到宝了，一插入吸管时容易弯折，只需用大拇指封住上端，就可以轻松他好二用剪刀给吸管，这样剪开剜着一下"
+        "，就能把下水道的头发轻松取出来，三用小刀把西瓜呈螺旋状还看用它来收纳家里的电线电池太方便了四吸管剪去两头。留下中间的小弹簧，封口没喝完的"
+        "酸奶很实用，关注我，了解更多生活小实验。\"，{}生成机器人推断出合理的\"{}\"为\"日常生活小妙招、生活小技巧、小妙招大全\""
+        "。".format(preference, preference),
+        "例如，给定一个视频，它的\"标题\"为\"长安系最便宜的轿车，4W起很多人都看不上它，但我知道车只是代步工具，又需要什么面子呢！ #长安汽车\"，"
+        "\"类别\"为\"汽车\"，\"ocr\"为\"长安系最便宜的一款轿车\"，\"asr\"为\"我不否认现在的国产和合资还有一定的差距，但确实是他们让我们5万开"
+        "了MP V8万开上了轿车，10万开张了ICV15万开张了大七座。\"，{}生成机器人推断出合理的\"{}\"为\"长安轿车报价、最便宜的长安轿车、新款长安轿"
+        "车\"。".format(preference, preference),
+        "例如，给定一个视频，它的\"标题\"为\"全屋嵌入式低音音响，主要是这个投影仪真的是爱了💕 \"，\"类别\"为\"房产家居\"，\"ocr\"为\"42平,一"
+        "室一厅小户型\"，\"asr\"为\"看，远方灯火闪亮着光。你一人低头在路上。这城市越大，越让人心慌多向往，多漫长。祝一路行李太多伤。把最初笑容都"
+        "淡忘。时光让我们变得脆弱，却坚强，让我在爱青青对你唱。我多想能多陪你唱。把什么生的风景对你讲。\"，{}生成机器人推断出合理的\"{}\"为\"小"
+        "户型装修、一室一厅装修、装修效果图\"。".format(preference, preference)
+    ]
+    sentences = []
+    prompt = PromptTemplate(
+        input_variables=["preference", "caption", "ocr_cover", "asr_pure", "category_name", "example"],
+        template="你是一个视频的{preference}生成机器人，根据输入的视频标题、类别、ocr、asr推理出合理的\"{preference}\"，以多个多"
+                 "于两字的标签形式进行表达，以顿号隔开。{example}那么，给定一个新的视频，它的\"标题\"为\"{caption}\"，\"类别\"为"
+                 "\"{category_name}\"，\"ocr\"为\"{ocr_cover}\"，\"asr\"为\"{asr_pure}\"，请推断出该视频的\"{preference}"
+                 "\"："
+    )
+    for ind, row in enumerate(tqdm.tqdm(data.iterrows())):
+        example = examples[random.randint(0, 4)]
+        caption = row[1]['caption'][:100]
+        ocr_cover = row[1]['ocr_cover'][:100]
+        asr_pure = row[1]['asr_pure'][:100]
+        text = prompt.format(
+            preference=preference,
+            caption=caption,
+            category_name=row[1]['category_name'],
+            ocr_cover=ocr_cover,
+            asr_pure=asr_pure, example=example
+        )
+
+        sentences.append(text)
+
+    f = open('../data/sentences.txt', 'w')
+    f.write("\n".join(sentences))
+    f.close()
 
 
 class Data:
@@ -35,8 +88,8 @@ def main():
     gen_feq = paras.gen_feq
 
     if func == "data_format":
-        data = Data(path=data_path).dataframe
-        print(data)
+        format_data(data=Data(path=data_path).dataframe, preference="兴趣标签")
+        print("Data formatting completed")
 
 
 
